a2113278db3e299ac951527f51816a7a81410df6
Shehaz Shefy <shehazshefy97@gmail.com>
2021-06-27 00:36:14 +0530

    4. To sort stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: { //Storybook -> Writing Stories -> Naming components and hierarchy -> Sorting stories
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
