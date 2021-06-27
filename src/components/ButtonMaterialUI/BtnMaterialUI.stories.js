cf5d42a17c49f9d1fd5e997f57fd4f3671419179
Shehaz Shefy <shehazshefy97@gmail.com>
2021-06-27 18:02:56 +0530

    9. Control Addon

--- src/components/ButtonMaterialUI/BtnMaterialUI.stories.js
+++ src/components/ButtonMaterialUI/BtnMaterialUI.stories.js
@@ -6,5 +6,23 @@ export default {
     component: Button
 }
 
-export const Primary = () => <Button variant="contained" color="primary">Primary</Button>
-export const Secondary = () => <Button variant="contained" color="secondary">Secondary</Button>
+//Args for trying Controls Addon
+
+const Template = args => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+    variant: 'contained',
+    color: 'primary',
+    children: 'Primary'
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+    variant: 'contained',
+    color: 'secondary',
+    children: 'Secondary'
+}
+
+// export const Primary = () => <Button variant="contained" color="primary">Primary</Button>
+// export const Secondary = () => <Button variant="contained" color="secondary">Secondary</Button>
